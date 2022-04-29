d8bac0858bccfd234d91ac8ef0f738aa55dfba96
Lee-Way Wang <lee.way56@comcast.net>
2022-04-29 15:46:48 -0700

    Create doc, include folders, add MIT License, update README

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
-# Name Popularity Visualizer
+# LWNamePopularityVisualizer
 
-This program takes a name and a gender as user input and displays the popularity and meaning of that name in the console.
+LWNamePopularityVisualizer visualizes the popularity of a name for each decade.
 
-Each number represents how popular a given name was for each decade, depending on a certain starting year. For example, a popularity of 1 (highest) would mean that that name was the 1st most popular name for that decade. A popularity of 1000 (lowest) would mean that that name was the 1000th most popular name for that decade.
+This program takes a name and a gender as user input and displays the popularity and meaning of
+that name.
 
-Then, the program plots the popularity of the name as a bar chart, with the height of each bar representing how popular the name was for a particular decade.
+This program plots the popularity of the name as a bar chart, with the height of each bar
+representing how popular the name was for a particular decade.
+
+The y-axis values represents how popular a given name was for each decade. For example, a
+popularity of 1 (highest) would mean that that name was the 1st most popular name for that decade.
+A popularity of 1000 (lowest) would mean that that name was the 1000th most popular name for that
+decade.
 
 ![Name meaning and popularity](https://github.com/leeway64/Name-Popularity-Visualizer/blob/master/Examples/Michelle%20name%20meaning%20and%20popularity.jpeg)
 ![Name plot](https://github.com/leeway64/Name-Popularity-Visualizer/blob/master/Examples/Michelle%20name%20plot.jpeg)
 
 
-I should note that the default name popularity file is names.txt with a start year of 1890. If names2.txt, then the start year should be 1863.
+The default name popularity file is names.txt with a start year of 1890. If the name popularity
+file is set to names2.txt, then the start year will be 1863.
 
-The name popularity information is based on [data from the Social Security Information](https://www.ssa.gov/OACT/babynames/).
 
-The specifications for this project was provided on the [UW Seattle CSE 142 summer 2017 website](https://courses.cs.washington.edu/courses/cse142/17su/homework.shtml). The original assignment was meant to be written in Java, but I completed the assignment using Python.
+
+
+
+## Third-Party Tools
+
+- [Matplotlib](https://matplotlib.org/): Plotting and visualization library for Python.
+
+
+## References
+
+The name popularity information is based on
+[data from the Social Security Information](https://www.ssa.gov/OACT/babynames/).
+
+The specifications for this project was provided on the
+[UW Seattle CSE 142 summer 2017 website](https://courses.cs.washington.edu/courses/cse142/17su/homework.shtml).
+The original assignment was meant to be written in Java, but I completed the assignment using Python.
---
+++ include/LWNamePopularityVisualizer.py
@@ -0,0 +1,104 @@
+# This programs prompts the user for a name and gender. If information on the name (with the respective gender)
+# exists, then the program will print the popularity and name meaning data. It will also draw a plot
+# representing the name's popularity.
+
+from matplotlib import pyplot as plt
+
+
+START_YEAR = 1890
+# START_YEAR = 1863
+BAR_WIDTH = 3
+
+
+# Prints an introductory message.
+def introduction():
+    print("This program allows you to search through the")
+    print("data from the Social Security Administration")
+    print("to see how popular a particular name has been")
+    print("since " + str(START_YEAR) + ".")
+
+
+# Searches the names or meanings file for the specific name and returns the entire line that contains the
+# name.
+# Inputs -
+# name (string): the name to search for
+# gender (string): the gender of the name to search for
+# text_file (string): the text file to search through
+# Returns -
+# The program returns the entire line that contains the name that it's searching for
+def search_file(name, gender, text_file):
+    with open(text_file) as file:
+        line = file.readline()
+        split_line = line.lower().split()
+        while line:
+            if split_line[0] == name.lower() and split_line[1] == gender.lower():
+                return line
+            line = file.readline()
+            split_line = line.lower().split()
+
+        return "\"" + name + "\" not found"
+
+
+# Creates a bar chart based on the popularity of each name.
+# Inputs -
+# name_popularity (list): a list of numbers representing the popularity of each name for every decade
+# name_meaning (string): the meaning of the name
+# No returns
+def draw_bar_chart(name_popularity, name_meaning):
+    popularity_data = name_popularity.split()
+    popularity_data = popularity_data[2:]
+    popularity_data = [int(z) for z in popularity_data]
+
+    # Popularity is mapped to the y-axis using the equation y_data = -popularity_data + 1001, assuming
+    # popularity_data is not 0. If popularity_data is 0, then y_data would also be 0.
+    # Basically, a popularity of 1 should map to a height of 1000, while a popularity of 1000 should map to
+    # a height of 1.
+    y_data = []
+    for i in range(0, len(popularity_data)):
+        if popularity_data[i] != 0:
+            y_data.append((-1*popularity_data[i])+1001)
+        else:
+            y_data.append(0)
+
+    year_data = [x*10 for x in range(0, len(popularity_data))]
+    year_data = [y+START_YEAR for y in year_data]
+
+    plt.style.use('fivethirtyeight')
+    plt.figure(name_meaning)
+    plt.bar(year_data, y_data, width=BAR_WIDTH)
+    plt.title('Popularity of name vs. years')
+
+    plt.xlabel('Years (decades)')
+    plt.xticks(ticks=year_data, labels=year_data)
+
+    plt.ylabel('Popularity of name (nth most popular)')
+    plt.yticks(ticks=[])
+
+    for i in range(0, len(year_data)):
+        plt.text(year_data[i], y_data[i], str(popularity_data[i]))
+
+    plt.grid(True)
+    plt.show()
+
+
+def main():
+    name_file = 'names.txt'  # Start date for first name file is 1890
+    # name_file = "names2.txt"  # Start date for second name file is 1863
+    meanings_file = "meanings.txt"
+
+
+    introduction()
+    name = input("Name: ")
+    gender = input("Gender (M or F): ")
+
+    name_popularity = search_file(name, gender, name_file)
+    print(name_popularity)
+
+    if name_popularity != "\"" + name + "\" not found":
+        name_meaning = search_file(name, gender, meanings_file)
+        print(name_meaning)
+        draw_bar_chart(name_popularity, name_meaning)
+
+
+if __name__ == '__main__':
+    main()
